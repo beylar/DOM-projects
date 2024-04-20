ca3e6916266c011e8721c50b05ed66eb55ebd5fa
Beyla Irakoze <beylaruz@gmail.com>
2024-04-20 11:08:26 +0200

    Clock

--- clock/script.js
+++ clock/script.js
@@ -4,4 +4,18 @@ doc.addEventListener('click', function (event){
     if(event.target.tagName === "BUTTON"){
         console.log("button " + event.target.innerText + " is clicked!")
     }
-})
+});
+//new clock
+function updateClock() {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+    const timeString = `${hours}:${minutes}:${seconds}`;
+
+    document.getElementById('clock').textContent = timeString;
+}
+
+setInterval(updateClock, 1000);
+
+updateClock();
